85b557bbf98c130c08cf56c7f6900d39440ab167
Ujjawal-Dudani <89912402+Ujjawal-Dudani@users.noreply.github.com>
2021-09-07 18:48:39 +0530

    Custom Listview

---
+++ app/src/main/java/com/example/customlistview/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.customlistview
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    val language = arrayOf<String>("C",
+        "C++",
+        "Java",
+        ".Net",
+        "Kotlin",
+        "Ruby",
+        "Rails",
+        "Python",
+        "Java Script",
+        "Php",
+        "Ajax",
+        "Perl",
+        "Hadoop")
+    val description = arrayOf<String>(
+        "C programming is considered as the base for other programming languages",
+        "C++ is an object-oriented programming language.",
+        "Java is a programming language and a platform.",
+        ".NET is a framework which is used to develop software applications.",
+        "Kotlin is a open-source programming language, used to develop Android apps and much more.",
+        "Ruby is an open-source and fully object-oriented programming language.",
+        "Ruby on Rails is a server-side web application development framework written in Ruby language.",
+        "Python is interpreted scripting  and object-oriented programming language.",
+        "JavaScript is an object-based scripting language.",
+        "PHP is an interpreted language, i.e., there is no need for compilation.",
+        "AJAX allows you to send and receive data asynchronously without reloading the web page.",
+        "Perl is a cross-platform environment used to create network and server-side applications.",
+        "Hadoop is an open source framework from Apache written in Java."
+    )
+
+    val imageId = arrayOf<Int>(
+        R.drawable.c, R.drawable.cpp, R.drawable.java,
+        R.drawable.net, R.drawable.kotlin, R.drawable.ruby,
+        R.drawable.rails, R.drawable.python, R.drawable.java_script,
+        R.drawable.php, R.drawable.java, R.drawable.python,
+        R.drawable.kotlin
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myListAdapter = MyListAdapter(this, language, description, imageId)
+        listView.adapter = myListAdapter
+
+        listView.setOnItemClickListener() { adapterView, view, position, id ->
+            val itemAtPos = adapterView.getItemAtPosition(position)
+            val itemIdAtPos = adapterView.getItemIdAtPosition(position)
+            Toast.makeText(this,
+                "Click on item at $itemAtPos its item id $itemIdAtPos",
+                Toast.LENGTH_LONG).show()
+        }
+    }
+}
